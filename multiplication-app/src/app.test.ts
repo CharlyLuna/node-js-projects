371a2bc53ca10d4a26a3fbdb916117ec0e7224e0
CharlyLuna <cluna10@ucol.mx>
2024-09-19 13:39:38 -0600

    Add last test to app file

--- multiplication-app/src/app.test.ts
+++ multiplication-app/src/app.test.ts
@@ -1,5 +1,19 @@
+import { ServerApp } from "./presentation/server-app"
+
 describe("Test on App", () => {
-  test("should be true", () => {
-    expect(true).toBe(true)
+  test("should call Server.run with values", async () => {
+    const serverRunMock = jest.fn()
+    ServerApp.run = serverRunMock
+    process.argv = ["node", "app.ts", "-b", "5", "-l", "5", "-n", "test-table"]
+
+    await import("./app")
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 5,
+      limit: 5,
+      showTable: false,
+      fileDestination: "outputs",
+      fileName: "test-table",
+    })
   })
 })
