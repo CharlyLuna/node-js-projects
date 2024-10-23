e42bec0ed49fec74ccd45b9d902296150c8f7411
CharlyLuna <charlyluna.dev44@gmail.com>
2024-10-23 16:09:29 -0600

    Implement checkService with file system datasource

--- NOC-app/src/domain/use-cases/checks/check-service.ts
+++ NOC-app/src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,5 @@
+import { LogEntity, LogSeverity } from "../../entities/log.entity"
+import { LogRepository } from "../../repository/log.repository"
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>
 }
@@ -6,6 +8,7 @@ type SuccesCallback = () => void
 type ErrorCallback = (err: string) => void
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccesCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -15,10 +18,17 @@ export class CheckService implements CheckServiceUseCase {
       const req = await fetch(url)
       if (!req.ok) throw new Error(`Error on check service: ${url}`)
 
+      const log = new LogEntity(LogSeverity.low, `Service ${url} is up`)
+
+      this.logRepository.saveLog(log)
       this.successCallback()
       return true
     } catch (err: any) {
-      this.errorCallback(err.message)
+      const message = `Error: ${err.message} on url: ${url}`
+      const log = new LogEntity(LogSeverity.high, message)
+      this.logRepository.saveLog(log)
+
+      this.errorCallback(message)
       return false
     }
   }
--- NOC-app/src/presentation/server.ts
+++ NOC-app/src/presentation/server.ts
@@ -1,14 +1,21 @@
 import { CheckService } from "../domain/use-cases/checks/check-service"
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource"
+import { LogRepositoryImpl } from "../infrastructure/repositories/log-impl.repository"
 import { CronService } from "./cron/cron-service"
+
+const logFileSystemRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+)
 
 export class Server {
   public static start() {
     console.log("Starting server...")
 
     CronService.createJob("*/10 * * * * *", () => {
-      const url = "https://www.google.com"
+      const url = "http://localhost:3000"
       // new CheckService().execute("http://localhost:3000")
       new CheckService(
+        logFileSystemRepository,
         () => console.log(url, "have succeded!"),
         (error) => console.log(error)
       ).execute(url)
